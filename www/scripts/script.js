96e35a9c16aceca388600822790648ae785766d6
Mira Naymyanova <mira71615@gmail.com>
2022-04-13 19:08:25 +0300

    правки после обновления пакета сборки

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,64 @@
-console.log('script work!');
+$(document).ready(function () {
+
+    let isOpen = false;
+  
+    $('.j-burger').on('click', function () {
+      if (isOpen) {
+        $('.j-menu').slideUp();
+        isOpen = false;
+      } else {
+        $('.j-menu').slideDown();
+        isOpen = true;
+      }
+  
+      $('.j-menu').slideToggle();
+    });
+  
+  
+  
+    // Аккордион Прайс
+  
+    let prevBtn;
+  
+    $('.j-accordion-btn').on('click', function () {
+  
+      if (prevBtn === $(this)[0]) {
+        $(this).next().slideToggle();
+        $(this).find('.j-prise-icon').toggleClass('prise-icon-minus');
+        return;
+      }
+  
+      $('.j-accordion-btn').next().slideUp();
+      $('.j-prise-icon').removeClass('prise-icon-minus');
+      $(this).find('.j-prise-icon').addClass('prise-icon-minus');
+      $(this).next().slideToggle();
+      prevBtn = $(this)[0];
+  
+    });
+  
+  // Аккордион FAQ
+
+  let prevBtnn;
+  
+  $('.j-accordion-btnn').on('click', function () {
+
+    if (prevBtnn === $(this)[0]) {
+      $(this).next().slideToggle();
+      $(this).find('.j-faq-icon').toggleClass('faq-icon-minus');
+      return;
+    }
+
+    $('.j-accordion-btnn').next().slideUp();
+    $('.j-faq-icon').removeClass('faq-icon-minus');
+    $(this).find('.j-faq-icon').addClass('faq-icon-minus');
+    $(this).next().slideToggle();
+    prevBtnn = $(this)[0];
+  });
+
+
+
+
+
+
+  
+  });
